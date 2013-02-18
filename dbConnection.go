718eb2268b863001df0248db326851758b9b25ec
jonesdeini <jonesdeini@gmail.com>
2013-02-18 17:49:17 -0500

    multi-row query

--- dbConnection.go
+++ dbConnection.go
@@ -6,33 +6,23 @@ import (
   "fmt"
 )
 
-type sqlObject struct {
-  Item ItemType
-}
-
-type ItemType struct {
-  Id int
-  Float_value float32
-  Defindex int
-  Quality int
-  Name string
-  Type string
-  CreatedAt string
-  UpdatedAt string
-  level int
-}
-
 func connect() {
   dbInfo := "username=" + dbUser() +
     " dbname=" + dbName() +
     " password=" + dbPass() +
-    " sslmode=disable"
-  fmt.Println(dbInfo)
+    " sslmode=" + dbSSLMode()
+  /* fmt.Println(dbInfo) */
   db, err := sql.Open("postgres", dbInfo)
   errorHandler(err)
-  /* var msg sqlObject */
-  var msg string
-  err = db.QueryRow("SELECT name FROM items").Scan(&msg)
+  defer db.Close()
+
+  rows, err := db.Query("SELECT name FROM items")
   errorHandler(err)
-  fmt.Println(msg)
+  var name string
+  for rows.Next() {
+    err = rows.Scan(&name)
+    errorHandler(err)
+    fmt.Println(name)
+  }
+
 }
