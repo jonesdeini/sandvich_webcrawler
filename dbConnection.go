97570063030563e644b5f860f8c5ef54e31f8c5a
jonesdeini <jonesdeini@gmail.com>
2013-03-10 23:08:38 -0400

    working (crappy) implementation of a mapping to an object

--- dbConnection.go
+++ dbConnection.go
@@ -6,6 +6,19 @@ import (
   "fmt"
 )
 
+  type dbObject struct {
+    Item DbItemsType
+  }
+
+  type DbItemsType struct {
+    Defindex   sql.NullInt64
+    FloatValue sql.NullFloat64 `sql:"float_value"`
+    Id         int
+    Level      sql.NullInt64
+    Name       string
+    Quality    sql.NullInt64
+  }
+
 func connect() {
   dbInfo := "username=" + dbUser() +
     " dbname=" + dbName() +
@@ -16,13 +29,13 @@ func connect() {
   errorHandler(err)
   defer db.Close()
 
-  rows, err := db.Query("SELECT name FROM items")
+  rows, err := db.Query("SELECT defindex, float_value, id, level, name, quality FROM items")
   errorHandler(err)
-  var name string
+  var dbType DbItemsType
   for rows.Next() {
-    err = rows.Scan(&name)
+    err = rows.Scan(&dbType.Defindex, &dbType.FloatValue, &dbType.Id, &dbType.Level, &dbType.Name, &dbType.Quality)
     errorHandler(err)
-    fmt.Println(name)
+    fmt.Println(dbType)
   }
 
 }
